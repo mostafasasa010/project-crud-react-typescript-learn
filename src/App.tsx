cefda508182ed74bbe6e94359308ae908ef75020
mostafasasa010 <mostafa.sasa20004@gmail.com>
2023-11-22 02:12:02 +0200

    Add Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import ProductCard from "./components/ProductCard";
+import { productList } from "./data";
 
-interface IProps {}
-
-const App = ({}: IProps) => {
+const App = () => {
+  // Renders
+  const products = productList.map((product) => (
+    <ProductCard key={product.id} product={product} />
+  ));
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 m-5 p-4 gap-2">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {products}
       </div>
     </div>
   );
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,21 +1,22 @@
+import { IProduct } from "../interfaces";
 import Image from "./Image";
 import Button from "./ui/Button";
 
-interface IProps {}
+interface IProps {
+  product: IProduct;
+}
 
-const ProductCard = ({}: IProps) => {
+const ProductCard = ({ product }: IProps) => {
+  const { title, description, imageURL, category } = product;
   return (
     <div className="p-2 border border-gray-300 rounded-md">
       <Image
-        imageURL="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        imageURL={imageURL}
         imageAlt="Product Pic"
         imageClass="rounded-md"
       />
-      <h4 className="text-lg font-semibold my-3">Nike Shoes</h4>
-      <p className="text-sm text-gray-500">
-        As luxury T-Shirt is just as distinctive and can be trimmed with premium
-        materia...
-      </p>
+      <h4 className="text-lg font-semibold my-3">{title}</h4>
+      <p className="text-sm text-gray-500">{description}</p>
       <div className="flex my-3 gap-2">
         <span className="w-5 h-5 bg-orange-600 rounded-full cursor-pointer" />
         <span className="w-5 h-5 bg-red-600 rounded-full cursor-pointer" />
@@ -24,9 +25,9 @@ const ProductCard = ({}: IProps) => {
       <div className="flex items-center justify-between">
         <span>$350</span>
         <div className="flex gap-2 items-center">
-          <span>Nike</span>
+          <span>{category.name}</span>
           <Image
-            imageURL="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            imageURL={category.imageURL}
             imageAlt="Product Pic"
             imageClass="w-10 h-10 rounded-full object-bottom"
           />
