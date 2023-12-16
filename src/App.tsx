2b2cb0531f8ce7f6cfd0163633c5d82b63e1fe37
mostafasasa010 <mostafa.sasa20004@gmail.com>
2023-12-16 19:01:28 +0200

    input ui component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import ProductCard from "./components/ProductCard";
 import Button from "./components/ui/Button";
 import Modal from "./components/ui/Modal";
-import { productList } from "./data";
+import { formInputsList, productList } from "./data";
+import Input from "./components/ui/Input";
 
 const App = () => {
   // States
@@ -12,13 +13,19 @@ const App = () => {
   function closeModal() {
     setIsOpen(false);
   }
-
   function openModal() {
     setIsOpen(true);
   }
+
   // Renders
-  const products = productList.map((product) => (
+  const renderProducts = productList.map((product) => (
     <ProductCard key={product.id} product={product} />
+  ));
+  const renderInputList = formInputsList.map((input, i) => (
+    <div key={i} className="flex flex-col">
+      <label htmlFor={input.id}>{input.label}</label>
+      <Input type={input.type} id={input.id} name={input.name} />
+    </div>
   ));
   return (
     <main className="container">
@@ -26,9 +33,10 @@ const App = () => {
         Bulid
       </Button>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 m-5 gap-2 md:gap-4">
-        {products}
+        {renderProducts}
       </div>
       <Modal isOpen={isOpen} closeModal={closeModal} title="Add New Product">
+        {renderInputList}
         <div className="flex items-center justify-between gap-2 mt-3">
           <Button classBtn="bg-indigo-700">Add</Button>
           <Button classBtn="bg-[#c2344d]" onClick={closeModal}>
